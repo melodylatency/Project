74ccf97cb19fe77bc553b856572d428912f2042a
melodylatency <melodylatency@yahoo.com>
2025-02-03 18:35:15 +0500

    height

--- frontend/src/components/Template.jsx
+++ frontend/src/components/Template.jsx
@@ -3,13 +3,16 @@ import { Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
 
-// import Rating from "./Rating";
-
 const Template = ({ template }) => {
   return (
     <Card className="my-3 p-3 rounded">
       <Link to={`/template/${template.id}`}>
-        <Card.Img src={template.image} variant="top" />
+        <Card.Img
+          className="h-[270px] object-cover"
+          src={template.image}
+          variant="top"
+          alt={template.title}
+        />
       </Link>
       <Card.Body>
         <Link to={`/template/${template.id}`}>
@@ -17,11 +20,9 @@ const Template = ({ template }) => {
             <h1 className="text-ellipsis truncate text-lg">{template.title}</h1>
           </Card.Title>
         </Link>
-
         <Card.Text className="pb-1" as="div">
-          <Rating value={4.7} text={`no comments`} />
+          <Rating value={4.7} text="no comments" />
         </Card.Text>
-
         <Card.Text as="h3">{template.topic}</Card.Text>
       </Card.Body>
     </Card>
