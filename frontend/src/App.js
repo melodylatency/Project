5f98e8a30ab54d5b1c78236edfde2f76e1b79cb9
melodylatency <melodylatency@yahoo.com>
2025-02-03 18:47:25 +0500

    fixed bg-height

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,10 +9,6 @@ const App = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      {/* 
-        The 'flex-grow' class makes the main area expand to fill the available space.
-        The background gradient will now cover the full height of this flexible area.
-      */}
       <main className="flex-grow bg-gradient-to-t from-teal-400 via-cyan-300 to-teal-400">
         <Container>
           <Outlet />
