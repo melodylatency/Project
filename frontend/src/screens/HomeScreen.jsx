4965b397757dbc928f33007cdcf44c926b4bc2ef
melodylatency <melodylatency@yahoo.com>
2025-02-08 16:54:46 +0500

    frontend brushup

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -37,16 +37,23 @@ const HomeScreen = () => {
         </Message>
       ) : (
         <>
-          <h1 className="text-center text-6xl py-5">Top Viewed Templates</h1>
-          <Row>
-            {top5.map((template) => (
-              <Col key={template.id} sm={12} md={6} lg={4} xl={4}>
-                <Template template={template} />
-              </Col>
-            ))}
-          </Row>
+          <h1 className="text-center text-6xl py-5">Top 5 Viewed Templates</h1>
+          {templates.length > 0 ? (
+            <Row>
+              {top5.map((template) => (
+                <Col key={template.id} sm={12} md={6} lg={4} xl={4}>
+                  <Template template={template} />
+                </Col>
+              ))}
+            </Row>
+          ) : (
+            <h1 className="flex justify-center text-3xl text-gray-500">
+              Nothing to see here...
+            </h1>
+          )}
 
           <h1 className="text-center text-6xl py-5">All other Templates</h1>
+
           <Link to={"/create"} className="flex flex-row justify-end">
             <Button
               className="flex justify-center"
@@ -56,13 +63,19 @@ const HomeScreen = () => {
               Create
             </Button>
           </Link>
-          <Row>
-            {restTemplates.map((template) => (
-              <Col key={template.id} sm={12} md={6} lg={4} xl={3}>
-                <Template template={template} />
-              </Col>
-            ))}
-          </Row>
+          {templates.length > 5 ? (
+            <Row>
+              {restTemplates.map((template) => (
+                <Col key={template.id} sm={12} md={6} lg={4} xl={3}>
+                  <Template template={template} />
+                </Col>
+              ))}
+            </Row>
+          ) : (
+            <h1 className="flex justify-center text-3xl text-gray-500">
+              Nothing to see here...
+            </h1>
+          )}
         </>
       )}
     </div>
