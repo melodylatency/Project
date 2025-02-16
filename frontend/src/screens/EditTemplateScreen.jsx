375b979a879c4fb3314575d0be67be198914bf8a
melodylatency <melodylatency@yahoo.com>
2025-02-17 01:43:26 +0500

    solid

--- frontend/src/screens/EditTemplateScreen.jsx
+++ frontend/src/screens/EditTemplateScreen.jsx
@@ -1,5 +1,16 @@
 import React, { useEffect, useState } from "react";
-import { Form, Button, Row, Col, Container, Card } from "react-bootstrap";
+import {
+  Form,
+  Button,
+  Row,
+  Col,
+  Container,
+  Card,
+  Tabs,
+  Tab,
+  Table,
+} from "react-bootstrap";
+import { FaChevronDown, FaChevronUp } from "react-icons/fa";
 import { DndContext, closestCenter } from "@dnd-kit/core";
 import {
   SortableContext,
@@ -19,10 +30,13 @@ import {
   useDeleteQuestionMutation,
   useUpdateQuestionMutation,
 } from "../redux/slices/questionsApiSlice";
+import { useGetTemplateFormsQuery } from "../redux/slices/formsApiSlice";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import Loader from "../components/Loader";
 import ReactMarkdown from "react-markdown";
 import "github-markdown-css/github-markdown-light.css";
+import moment from "moment";
+import Message from "../components/Message";
 
 const EditTemplateScreen = () => {
   const { id: templateId } = useParams();
@@ -32,6 +46,12 @@ const EditTemplateScreen = () => {
     isLoading: isLoadingTemplate,
     refetch: refetchTemplate,
   } = useGetTemplateByIdQuery(templateId);
+
+  const {
+    data: forms,
+    isLoading: loadingForms,
+    error: errorForm,
+  } = useGetTemplateFormsQuery(templateId);
 
   const [createQuestion, { isLoading: isCreatingQuestion }] =
     useCreateQuestionMutation();
@@ -46,8 +66,12 @@ const EditTemplateScreen = () => {
 
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
+  const [access, setAcess] = useState("public");
+  const [topic, setTopic] = useState("Other");
+  const [topicList] = useState(["Other", "Education", "Poll"]);
   const [questionList, setQuestionList] = useState([]);
   const [editingQuestionId, setEditingQuestionId] = useState(null);
+  const [sortOrderForms, setSortOrderForms] = useState("desc");
   const [newQuestion, setNewQuestion] = useState({
     type: "text",
     title: "",
@@ -64,6 +88,8 @@ const EditTemplateScreen = () => {
       setTitle(template.title);
       setDescription(template.description);
       setQuestionList(template.questionList);
+      setAcess(template.access);
+      setTopic(template.topic);
     }
   }, [template]);
 
@@ -196,10 +222,12 @@ const EditTemplateScreen = () => {
       await updateTemplate({
         title,
         description,
-        topic: "Other",
+        access,
+        topic,
         templateId,
       }).unwrap();
       refetch();
+      refetchTemplate();
       navigate("/");
       toast.success("Template updated successfully!");
     } catch (err) {
@@ -214,147 +242,255 @@ const EditTemplateScreen = () => {
       </Link>
       <h1 className="text-center mb-4 text-5xl">Edit Template</h1>
       {/* Move DndContext outside the Form */}
-      <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
-        <Form onSubmit={handleSubmit}>
-          {/* Form Title */}
+      <Tabs
+        defaultActiveKey="general"
+        id="uncontrolled-tab-example"
+        className="mb-3"
+      >
+        <Tab eventKey="general" title="General Settings">
+          <Form onSubmit={handleSubmit}>
+            {/* Form Title */}
 
-          <Form.Group className="mb-3" controlId="formTitle">
-            <Form.Label className="fs-4">Template Title</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Enter a title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              size="lg"
-            />
-          </Form.Group>
+            <Form.Group className="mb-3" controlId="formTitle">
+              <Form.Label className="fs-4">Template Title</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter a title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                size="lg"
+              />
+            </Form.Group>
 
-          {/* Form Description with Markdown Support */}
-          <Form.Group className="mb-4" controlId="formDescription">
-            <Form.Label className="fs-5">
-              Template Description (supports Markdown)
-            </Form.Label>
-            <Form.Control
-              as="textarea"
-              rows={10}
-              placeholder="Enter a description (optional, Markdown supported)"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-            />
-          </Form.Group>
+            {/* Form Description with Markdown Support */}
+            <Form.Group className="mb-4" controlId="formDescription">
+              <Form.Label className="fs-5">
+                Template Description (supports Markdown)
+              </Form.Label>
+              <Form.Control
+                as="textarea"
+                rows={10}
+                placeholder="Enter a description (optional, Markdown supported)"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </Form.Group>
 
-          {/* Markdown Preview */}
-          <Card className="mb-4">
-            <Card.Header>Markdown Preview</Card.Header>
-            <Card.Body>
-              <div className="markdown-body">
-                <ReactMarkdown>{description}</ReactMarkdown>
-              </div>
-            </Card.Body>
-          </Card>
+            {/* Markdown Preview */}
+            <Card className="mb-4">
+              <Card.Header>Markdown Preview</Card.Header>
+              <Card.Body>
+                <div className="markdown-body">
+                  <ReactMarkdown>{description}</ReactMarkdown>
+                </div>
+              </Card.Body>
+            </Card>
 
-          {/* Questions List */}
-          <h4 className="mb-3">Questions</h4>
-          <SortableContext
-            items={questionList.map((q) => q.id)}
-            strategy={verticalListSortingStrategy}
+            <Form.Group className="flex flex-row justify-between">
+              <Form.Select
+                name="access"
+                value={access}
+                onChange={(e) => setAcess(e.target.value)}
+                className="w-1/3"
+              >
+                <option value="public">Public</option>
+                <option value="restricted">Restricted</option>
+              </Form.Select>
+
+              <Form.Select
+                name="topic"
+                value={topic}
+                onChange={(e) => setTopic(e.target.value)}
+                className="w-1/3"
+              >
+                {topicList.map((topic) => (
+                  <option value={topic}>{topic}</option>
+                ))}
+              </Form.Select>
+            </Form.Group>
+            <div className="text-center mt-2">
+              <Button variant="primary" type="submit" size="lg">
+                Save Form
+              </Button>
+            </div>
+          </Form>
+        </Tab>
+
+        <Tab eventKey="questions" title="Questions">
+          <DndContext
+            collisionDetection={closestCenter}
+            onDragEnd={handleDragEnd}
           >
-            {questionList.map((question, index) => (
-              <SortableItem key={question.id} id={question.id} index={index}>
-                <QuestionCard
-                  question={question}
-                  index={index}
-                  onDelete={() => handleDelete(question.id)}
-                  onUpdate={() => setEditingQuestionId(question.id)}
-                  isEditing={question.id === editingQuestionId}
-                  onSave={handleUpdate}
-                  onCancelEdit={() => setEditingQuestionId(null)}
-                  dragHandleProps={undefined}
-                />
-              </SortableItem>
-            ))}
-          </SortableContext>
+            {/* Questions List */}
+            <h4 className="mb-3">Questions</h4>
+            <SortableContext
+              items={questionList.map((q) => q.id)}
+              strategy={verticalListSortingStrategy}
+            >
+              {questionList.map((question, index) => (
+                <SortableItem key={question.id} id={question.id} index={index}>
+                  <QuestionCard
+                    question={question}
+                    index={index}
+                    onDelete={() => handleDelete(question.id)}
+                    onUpdate={() => setEditingQuestionId(question.id)}
+                    isEditing={question.id === editingQuestionId}
+                    onSave={handleUpdate}
+                    onCancelEdit={() => setEditingQuestionId(null)}
+                    dragHandleProps={undefined}
+                  />
+                </SortableItem>
+              ))}
+            </SortableContext>
 
-          {/* Add New Question */}
-          <Card className="mt-4 mb-4 shadow-sm">
-            <Card.Header className="bg-light">Add New Question</Card.Header>
-            <Card.Body>
-              <Row className="g-3 align-items-center">
-                <Col md={6}>
-                  <Form.Control
-                    type="text"
-                    placeholder="Enter the question"
-                    value={newQuestion.title}
-                    onChange={(e) =>
-                      setNewQuestion({ ...newQuestion, title: e.target.value })
-                    }
-                  />
-                </Col>
-                <Col md={3}>
-                  <Form.Select
-                    value={newQuestion.type}
-                    onChange={(e) =>
-                      setNewQuestion({ ...newQuestion, type: e.target.value })
-                    }
-                  >
-                    <option value="SINGLE_LINE">Single-line</option>
-                    <option value="MULTI_LINE">Multi-line</option>
-                    <option value="INTEGER">Integer</option>
-                    <option value="CHECKBOX">Checkbox</option>
-                  </Form.Select>
-                </Col>
-                <Col md={2}>
-                  <Form.Check
-                    type="checkbox"
-                    label="Display"
-                    checked={newQuestion.displayOnTable}
-                    onChange={(e) =>
-                      setNewQuestion({
-                        ...newQuestion,
-                        displayOnTable: e.target.checked,
-                      })
-                    }
-                  />
-                </Col>
-                <Col md={1}>
-                  <Button variant="primary" onClick={addQuestion}>
-                    +
-                  </Button>
-                </Col>
-              </Row>
-              <Row className="my-3">
-                <Col md={12}>
-                  <Form.Control
-                    as={"textarea"}
-                    placeholder="Optional description"
-                    value={newQuestion.description}
-                    onChange={(e) =>
-                      setNewQuestion({
-                        ...newQuestion,
-                        description: e.target.value,
-                      })
-                    }
-                  />
-                </Col>
-              </Row>
-              <Form.Text className="text-muted">
-                Note: You can add up to 4 questionList of each type.
-              </Form.Text>
-            </Card.Body>
-          </Card>
+            {/* Add New Question */}
+            <Card className="mt-4 mb-4 shadow-sm">
+              <Card.Header className="bg-light">Add New Question</Card.Header>
+              <Card.Body>
+                <Row className="g-3 align-items-center">
+                  <Col md={6}>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter the question"
+                      value={newQuestion.title}
+                      onChange={(e) =>
+                        setNewQuestion({
+                          ...newQuestion,
+                          title: e.target.value,
+                        })
+                      }
+                    />
+                  </Col>
+                  <Col md={3}>
+                    <Form.Select
+                      value={newQuestion.type}
+                      onChange={(e) =>
+                        setNewQuestion({ ...newQuestion, type: e.target.value })
+                      }
+                    >
+                      <option value="SINGLE_LINE">Single-line</option>
+                      <option value="MULTI_LINE">Multi-line</option>
+                      <option value="INTEGER">Integer</option>
+                      <option value="CHECKBOX">Checkbox</option>
+                    </Form.Select>
+                  </Col>
+                  <Col md={2}>
+                    <Form.Check
+                      type="checkbox"
+                      label="Display"
+                      checked={newQuestion.displayOnTable}
+                      onChange={(e) =>
+                        setNewQuestion({
+                          ...newQuestion,
+                          displayOnTable: e.target.checked,
+                        })
+                      }
+                    />
+                  </Col>
+                  <Col md={1}>
+                    <Button variant="primary" onClick={addQuestion}>
+                      +
+                    </Button>
+                  </Col>
+                </Row>
+                <Row className="my-3">
+                  <Col md={12}>
+                    <Form.Control
+                      as={"textarea"}
+                      placeholder="Optional description"
+                      value={newQuestion.description}
+                      onChange={(e) =>
+                        setNewQuestion({
+                          ...newQuestion,
+                          description: e.target.value,
+                        })
+                      }
+                    />
+                  </Col>
+                </Row>
+                <Form.Text className="text-muted">
+                  Note: You can add up to 4 questionList of each type.
+                </Form.Text>
+              </Card.Body>
+            </Card>
+            {(isLoading ||
+              isDeleting ||
+              isUpdatingQuestion ||
+              isLoadingTemplate ||
+              isCreatingQuestion) && <Loader />}
+          </DndContext>
+        </Tab>
 
-          <div className="text-center">
-            <Button variant="primary" type="submit" size="lg">
-              Save Form
-            </Button>
-          </div>
-          {(isLoading ||
-            isDeleting ||
-            isUpdatingQuestion ||
-            isLoadingTemplate ||
-            isCreatingQuestion) && <Loader />}
-        </Form>
-      </DndContext>
+        <Tab eventKey="results" title="Results">
+          {loadingForms ? (
+            <Loader />
+          ) : errorForm ? (
+            <Message>{errorForm?.data?.message || errorForm.error}</Message>
+          ) : forms.length === 0 ? (
+            <Message>No forms found.</Message>
+          ) : (
+            <>
+              <Table striped hover responsive className="table-sm">
+                <thead>
+                  <tr>
+                    <th className="text-nowrap">USER ID</th>
+                    <th className="text-nowrap">FORM ID</th>
+                    <th
+                      className="min-w-[120px] cursor-pointer"
+                      onClick={() =>
+                        setSortOrderForms((prev) =>
+                          prev === "asc" ? "desc" : "asc"
+                        )
+                      }
+                    >
+                      <div className="d-flex align-items-center gap-1 text-nowrap">
+                        DATE FILLED
+                        {sortOrderForms === "asc" ? (
+                          <FaChevronUp className="text-muted" />
+                        ) : (
+                          <FaChevronDown className="text-muted" />
+                        )}
+                      </div>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {forms
+                    .slice()
+                    .sort((a, b) => {
+                      const dateA = new Date(a.createdAt);
+                      const dateB = new Date(b.createdAt);
+                      return sortOrderForms === "asc"
+                        ? dateA - dateB
+                        : dateB - dateA;
+                    })
+                    .map((form) => (
+                      <tr key={form.id}>
+                        <td>{form.user_id}</td>
+                        <td>
+                          <Link
+                            to={`/form/${form.id}`}
+                            className="text-blue-500 underline"
+                          >
+                            {form.id}
+                          </Link>
+                        </td>
+                        <td className="text-nowrap">
+                          {moment(form.createdAt).format(
+                            "MMMM Do YYYY, h:mm:ss a"
+                          )}
+                        </td>
+                      </tr>
+                    ))}
+                </tbody>
+              </Table>
+            </>
+          )}
+        </Tab>
+        <Tab eventKey="aggregation" title="Aggregation">
+          Tab content for aggregation
+        </Tab>
+      </Tabs>
     </Container>
   );
 };
