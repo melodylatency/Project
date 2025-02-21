db6522aedaa7982d2ac56d7dc11e9ba567656c30
melodylatency <melodylatency@yahoo.com>
2025-02-21 21:12:58 +0500

    base

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-      <main className="flex-grow bg-gradient-to-t from-teal-400 via-cyan-300 to-teal-400">
+      <main className="flex-grow bg-gradient-to-t from-teal-400 via-cyan-300 to-teal-400 dark:from-gray-800 dark:via-gray-700 dark:to-gray-800">
         <Container>
           <Outlet />
         </Container>
